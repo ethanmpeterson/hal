fd013636493c761a56122130c0b4eb6b7e6e5869
ologush <17ojl@queensu.ca>
2025-01-15 23:49:18 -0500

    Fixed compilation issues

--- include/hal_i2c.h
+++ include/hal_i2c.h
@@ -9,25 +9,29 @@
 extern "C" {
 #endif
 
-    //Pointers to stuff on specific channel peripherals
-    
     typedef struct {
         hal_error_E (*initChannel)(void);
         hal_error_E (*sendData)(uint16_t address, uint8_t* data, uint8_t nBytes);
         hal_error_E (*receiveData)(uint16_t address, uint8_t* data, uint8_t nBytes);
+        hal_error_E (*IRQ_Handler)(void);
+        hal_error_E (*receiveNextByte)(uint8_t *receivedByte);
+        hal_error_E (*sendNextByte)(uint8_t byteToSend);
+        hal_error_E (*beginSend)(uint16_t address, uint8_t nBytes);
+        hal_error_E (*beginReceive)(uint16_t address, uint8_t nBytes);
+        hal_error_E (*endTransmission)(void);
+
     } hal_i2c_channelConfig_S;
 
-    //
     typedef struct {
-        hal_i2c_channelConfig_S const * const channels; //Array of channel configs
+        hal_i2c_channelConfig_S const * const channels;
         const uint8_t channelCount;
     } hal_i2c_config_S;
 
 
-
     hal_error_E hal_i2c_init(hal_i2c_config_S const * const config);
     hal_error_E hal_i2c_sendData(hal_i2c_channel_E channel, uint16_t address, uint8_t* data, uint8_t nBytes);
-    hal_error_E hal_i2c_receiveData(hal_i2c_channel_E channel, uint16_t address, uint8_t data, uint8_t nBytes);
+    hal_error_E hal_i2c_receiveData(hal_i2c_channel_E channel, uint16_t address, uint8_t* data, uint8_t nBytes);
+    hal_error_E hal_i2c_IRQHandler(hal_i2c_channel_E);
 
     
     typedef enum {
@@ -48,6 +52,13 @@ extern "C" {
         HAL_I2C_MODE_SLAVE_RECEIVER
     } hal_i2c_mode_E;
 
+    typedef enum {
+        HAL_I2C_TRANSFER,
+        HAL_I2C_RECEIVE,
+        HAL_I2C_NOOP
+    } hal_i2c_transfer_type_E;
+
 #ifdef __cplusplus
 }
+#endif
 #endif
--- src/hal_i2c.c
+++ src/hal_i2c.c
@@ -2,9 +2,16 @@
 
 #include "hal_i2c.h"
 #include "hal.h"
+#include <stdbool.h>
 
 typedef struct {
     hal_i2c_config_S const * config;
+    volatile uint8_t receiveData[HAL_I2C_CHANNEL_COUNT][0xFF];
+    volatile uint8_t sendData[HAL_I2C_CHANNEL_COUNT][0xFF];
+    volatile uint8_t dataIndex[HAL_I2C_CHANNEL_COUNT];
+    volatile bool isSending[HAL_I2C_CHANNEL_COUNT];
+    volatile bool isReceiving[HAL_I2C_CHANNEL_COUNT];
+    volatile uint8_t* receiveAddress[HAL_I2C_CHANNEL_COUNT];
 } hal_i2c_config_data_S;
 
 static hal_i2c_config_data_S hal_i2c_config_data;
@@ -33,9 +40,55 @@ hal_error_E hal_i2c_init(hal_i2c_config_S const *const config) {
 }
 
 hal_error_E hal_i2c_sendData(hal_i2c_channel_E channel, uint16_t address, uint8_t* data, uint8_t nBytes) {
-    return config_data->config->channels[channel].sendData(address, &data, nBytes);
+    return config_data->config->channels[channel].sendData(address, data, nBytes);
 }
 
 hal_error_E hal_i2c_receiveData(hal_i2c_channel_E channel, uint16_t address, uint8_t* data, uint8_t nBytes) {
-    return config_data->config->channels[channel].sendData(address, &data, nBytes);
+    return config_data->config->channels[channel].sendData(address, data, nBytes);
+}
+
+hal_error_E hal_i2c_IRQHandler(hal_i2c_channel_E channel) {
+    return config_data->config->channels[channel].IRQ_Handler();
+}
+
+hal_error_E hal_i2c_data_transfer_complete(hal_i2c_channel_E channel) {
+    if (config_data->isSending[channel]) {
+
+        //Reset data buffer
+        memset(config_data->sendData[channel], 0, 0xFF);
+        config_data->dataIndex[channel] = 0;
+        config_data->isSending[channel] = false;
+        config_data->config->channels[channel].endTransmission();
+    }
+    if (config_data->isReceiving[channel]) {
+        //Place data in frame passed when I2C began receiving, can maybe remove and just place data in that frame as it comes in
+        memcpy(config_data->receiveData, config_data->receiveAddress[channel], ++config_data->dataIndex[channel]);
+
+        memset(config_data->receiveData[channel], 0, 0xFF);
+        config_data->dataIndex[channel] = 0;
+        config_data->isReceiving[channel] = false;
+        config_data->config->channels[channel].endTransmission();
+
+    }
+}
+
+hal_error_E hal_i2c_begin_send(hal_i2c_channel_E channel, uint16_t address, uint8_t nBytes) {
+    config_data->isSending[channel] = true;
+    config_data->config->channels[channel].beginSend(address, nBytes);   
+}
+
+hal_error_E hal_i2c_begin_receive(hal_i2c_channel_E channel, uint16_t address, uint8_t nBytes, uint8_t* returnAddress) {
+    config_data->isReceiving[channel] = true;
+    config_data->receiveAddress[channel] = returnAddress;
+    config_data->config->channels[channel].beginReceive(address, nBytes);
+}
+
+hal_error_E hal_i2c_receive_next_byte(hal_i2c_channel_E channel) {
+    config_data->config->channels[channel].receiveNextByte(&config_data->receiveData[channel][config_data->dataIndex[channel]]);
+    config_data->dataIndex[channel]++;
+}
+
+hal_error_E hal_i2c_transmit_next_byte(hal_i2c_channel_E channel) {
+    config_data->config->channels[channel].sendNextByte(config_data->sendData[channel][config_data->dataIndex[channel]]);
+    config_data->dataIndex[channel]++;
 }
