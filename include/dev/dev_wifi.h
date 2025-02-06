85953ef377956ee49e045ed9fbc2d9d74ac22fa2
ologush <17ojl@queensu.ca>
2025-02-05 20:30:46 -0500

    Added code to set the time. Also added placeholders for remaining functions. Will fill them in once in sync with latest RTC updates

--- include/dev/dev_wifi.h
+++ include/dev/dev_wifi.h
@@ -8,17 +8,53 @@
 #include "hal_uart.h"
 #include "hal_uart_microSpecific.h"
 
+#define DEV_WIFI_MAX_COMMAND_LENGTH 7
+#define DEV_WIFI_MAX_COMMAND_ARGS   6
+#define DEV_WIFI_MAX_ARG_LENGTH     1
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+//This stores all the command types. 
+//Since the commands will be sent from the ESP, there is no need for the command identifier to be human readable
+typedef enum {
+
+    DEV_WIFI_COMMAND_SET_TIME,
+    DEV_WIFI_COMMAND_GET_TIME,
+    DEV_WIFI_COMMAND_SET_ALARM,
+    DEV_WIFI_COMMAND_GET_ALARMS,
+    DEV_WIFI_COMMAND_REMOVE_ALARM
+
+} dev_wifi_command_id_E;
+
+
 typedef struct {
 
+    dev_wifi_command_id_E id;
+    
+    // function pointer to the code that processes the command,
+    // the callback is called with an array of arguments and the number of args.
+    // The array of arguments includes everything after the ID. For example if the id was 1 and you sen the command "1 x y", the arg array would be ["x", "y"] and args would be 2.
+    hal_error_E (*callback)(char **arg, uint32_t args);
+} dev_wifi_command_S;
+
+typedef struct {
+    // The UART channel used to collect and parse command strings
     const hal_uart_channel_E wifiPort;
 
+    // Lookup table of the command callbacks to parse strings
+    dev_wifi_command_S const * const commands;
+    const uint8_t commandCount;
 } dev_wifi_config_S;
 
 hal_error_E dev_wifi_init(dev_wifi_config_S const *const config);
+
+static hal_error_E dev_wifi_setTime(uint8_t *commandArray);
+static hal_error_E dev_wifi_getTime(void);
+static hal_error_E dev_wifi_getAlarms(void);
+static hal_error_E dev_wifi_removeAlarm(uint8_t *commandArray);
+static hal_error_E dev_wifi_setAlarm(uint8_t *commandArray);
 
 #ifdef __cplusplus
 }
--- src/dev/dev_wifi.c
+++ src/dev/dev_wifi.c
@@ -6,8 +6,96 @@
 #include "dev_wifi.h"
 #include "hal_uart_microSpecific.h"
 
+#include "hal_rtc.h"
+
 typedef struct {
+
+    char commandBuffer[DEV_WIFI_MAX_COMMAND_LENGTH];
+
+    char commandTokens[DEV_WIFI_MAX_COMMAND_ARGS][DEV_WIFI_MAX_ARG_LENGTH];
 
     dev_wifi_config_S const * config;
 
 } dev_wifi_data_S;
+
+static dev_wifi_data_S dev_wifi_data;
+static dev_wifi_data_S * const data = &dev_wifi_data;
+
+hal_error_E dev_wifi_init(dev_wifi_config_S const *const config) {
+    memset(data, 0U, sizeof(dev_wifi_data_S));
+    
+    if (config == NULL) {
+        return HAL_ERROR_ERR;
+    }
+
+    data->config = config;
+
+    return HAL_ERROR_OK;
+}
+
+// Array should start with a unique beginning and ending byte. This is to ensure it is receiving a full command
+hal_error_E dev_wifi_processCommandArray(uint8_t *commandArray) {
+
+    switch(commandArray[0]) {
+        case DEV_WIFI_COMMAND_SET_TIME:
+            dev_wifi_setTime(commandArray);
+            break;
+
+        case DEV_WIFI_COMMAND_GET_TIME:
+            dev_wifi_getTime();
+            break;
+        
+        case DEV_WIFI_COMMAND_GET_ALARMS:
+            dev_wifi_getAlarms();
+            break;
+
+        case DEV_WIFI_COMMAND_REMOVE_ALARM:
+            dev_wifi_removeAlarm(commandArray);
+            break;
+
+        case DEV_WIFI_COMMAND_SET_ALARM:
+            dev_wifi_setAlarm(commandArray);
+            break;
+        
+        default:
+            return HAL_ERROR_ERR;
+            break;
+    }
+}
+
+// Set time sent from wifi board
+static hal_error_E dev_wifi_setTime(uint8_t *commandArray) {
+
+    //Create the structure to pass to the hal_rtc function to set the time
+    hal_rtc_time_S timeToSet = {
+        .year = commandArray[1],
+        .month = commandArray[2],
+        .day = commandArray[3],
+
+        .weekday = commandArray[4],
+        .hour = commandArray[5],
+        .minute = commandArray[6],
+        .seconds = commandArray[7]
+    };
+
+    hal_error_E ret = hal_rtc_setTime(&timeToSet);
+
+    return ret;
+}
+
+//Fill in the rest of these functions on each of their own branches
+static hal_error_E dev_wifi_getTime(void) {
+
+}
+
+static hal_error_E dev_wifi_getAlarms(void) {
+
+}
+
+static hal_error_E dev_wifi_removeAlarm(uint8_t *commandArray) {
+
+}
+
+static hal_error_E dev_wifi_setAlarm(uint8_t *commandArray) {
+
+}
